710376f20fbed845f3c7500fd676631ef6f98a2d
skhabiri <shahnam.khabiri@gmail.com>
2020-08-24 16:22:41 -0700

    we get a 404 error

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ import uvicorn
 from app.api import predict, viz
 
 app = FastAPI(
-    title='DS API',
-    description='Lorem ipsum',
+    title='Spotify API',
+    description='Fast-API interface for Spotify application',
     version='0.1',
     docs_url='/',
 )
