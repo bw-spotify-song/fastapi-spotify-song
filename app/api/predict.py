b67eb9c95f755d731d95427397b81efb960edf3c
skhabiri <shahnam.khabiri@gmail.com>
2020-08-26 15:08:12 -0700

    bw spotify api work complete

--- app/api/predict.py
+++ app/api/predict.py
@@ -8,8 +8,8 @@ import joblib
 # from sklearn.neighbors import NearestNeighbors
 
 
-FILENAME = "./app/api/KNN_final.joblib"
-csv_url = "https://raw.githubusercontent.com/bw-spotify-song/fastapi-spotify-song/main/etc/new_df.csv"
+FILENAME = "./app/api/BW_Spotify_KNN.joblib"
+csv_url = "./app/api/BW_Spotify_DF.csv"
 
 log = logging.getLogger(__name__)
 router = APIRouter()
@@ -32,7 +32,7 @@ async def predict(id):
 
 
     knn = joblib.load(FILENAME)
-    new_df = pd.read_csv(csv_url)
+    df = pd.read_csv(csv_url)
 
     def predict_model(track_id, new_df):
         obs = new_df.index[new_df['id'] == track_id]
@@ -42,17 +42,18 @@ async def predict(id):
         new_obs = neighbors[1][0][6:20]
         return list(new_df.loc[new_obs, 'id'])
 
+    # id1 ='6QBInZBkQNIQYU9gGzT5l4'
     id1 = '6NxAf7M8DNHOBTmEd3JSO5'
     # id1 = [-1,'73LY41HCJzlQwoNBmWM7Md']
     # id1 = id1.reshape(1,-1)
-    print("##############\n", new_df.shape, len(id1))
+    print("##############\n", df.shape, len(id1))
     # load(.file.cs)
     # X_new = to_df(song)
     # log.info(X_new)
     # y_pred = random.choice([True, False])
     # y_pred_proba = random.random() / 2 + 0.5
 
-    trackids = predict_model(track_id= id1, new_df=new_df)
-    return #{
-        # 'Suggested track IDs': trackids
-        # }
+    trackids = predict_model(track_id= id1, new_df=df)
+    return {
+         'Suggested track IDs': trackids
+         }
