5aa20798ee07fb089e892c2121346aaae90006b3
skhabiri <shahnam.khabiri@gmail.com>
2020-08-26 11:45:12 -0700

    model related stuff added

--- app/api/predict.py
+++ app/api/predict.py
@@ -3,16 +3,20 @@ import random
 from fastapi import APIRouter
 from pydantic import BaseModel, Field, validator
 from .fedata import *
-# import joblib
-FILENAME = 'model-v1.sav'
+# import jimport jobliboblib
+import joblib
+# from sklearn.neighbors import NearestNeighbors
 
+
+FILENAME = "./app/api/KNN.joblib"
+csv_url = "https://raw.githubusercontent.com/bw-spotify-song/DS/master/data_w_genres.csv"
 
 log = logging.getLogger(__name__)
 router = APIRouter()
 
 
 @router.post('/predict')
-async def predict(id: int):
+async def predict(id):
     """
     takes song id from front end and returns a list
     of song ids that are closer to the one
@@ -26,11 +30,26 @@ async def predict(id: int):
     - `suggestion`: a list of song ids that are similar to the user's
     """
 
-    X_new = to_df(song)
-    log.info(X_new)
-    y_pred = random.choice([True, False])
-    y_pred_proba = random.random() / 2 + 0.5
+
+    knn = joblib.load(FILENAME)
+    new_df = pd.read_csv(csv_url)
+
+    def predict(track_id, new_df):
+        obs = new_df.index[new_df['id'] == track_id]
+        series = new_df.iloc[obs, 5:].to_numpy()
+
+        neighbors = knn.kneighbors(series)
+        new_obs = neighbors[1][0][6:20]
+        return list(new_df.loc[new_obs, 'id'])
+
+    # predict('6NxAf7M8DNHOBTmEd3JSO5')
+    # load(.file.cs)
+    # X_new = to_df(song)
+    # log.info(X_new)
+    # y_pred = random.choice([True, False])
+    # y_pred_proba = random.random() / 2 + 0.5
+
+    trackids = predict(id)
     return {
-        'prediction': y_pred,
-        'probability': y_pred_proba
-    }
+        'Suggested track IDs': trackids
+        }
